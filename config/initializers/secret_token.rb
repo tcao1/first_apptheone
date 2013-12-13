31a03f8d614bc416f25b99dc50354f5a1d52d86a
Tony <Tcao@chicagotechacademy.org>
2013-12-13 13:09:20 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApptheone::Application.config.secret_token = '6bb08cfc6796c49e8421aa1d586e4b300e9c24a73675621cb231f6085eaa82a3b4092f13b6ac8033a6a3cbc3f0619253eb00687d96d60e4d72b8a6a53242d842'
